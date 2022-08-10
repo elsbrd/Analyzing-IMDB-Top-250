502d50e2d242e174261b5db79f1997769fd8ee92
elsbrd <56909624+elsbrd@users.noreply.github.com>
2022-08-10 19:36:54 +0300

    Update film.py
    Style improvements

--- film.py
+++ film.py
@@ -1,54 +1,53 @@
 from bs4 import BeautifulSoup
 import requests
 import json
-# title, picture url, position in top 250, year released, rating, url inside the title
+from urllib.parse import urljoin
+
+url = 'http://www.imdb.com'
 
 
 def get_movie_soup():
-    url = 'http://www.imdb.com/chart/top'
-    page = requests.get(url)
+    path = urljoin(url, '/chart/top')
+    page = requests.get(path)
     soup = BeautifulSoup(page.text, 'html.parser')
-    table = soup.find('tbody', attrs={'class': 'lister-list'})
-    rows = table.find_all('tr')
+    get_tbody = soup.find('tbody', attrs={'class': 'lister-list'})
+    rows = get_tbody.find_all('tr')
     return rows
 
-def get_imd_movies(i):
-    movie = i.find_all('td', class_='titleColumn')[0]
-    image_url = i.find_all('td', class_='posterColumn')[0].a.img.get('src')
-    rating = i.find_all_next('td', class_='ratingColumn imdbRating')[0].strong.text
-    return movie, image_url, rating
+
+def parse_movie_row(row):
+    movie = row.find_all('td', class_='titleColumn')[0]
+    image_url = row.find_all('td', class_='posterColumn')[0].a.img.get('src')
+    rating = row.find_all('td', class_='ratingColumn imdbRating')[0].strong.text
+    title, movie_url, year, position = get_movie_table_rows(movie)
+    return title, movie_url, year, image_url, position,  rating
 
 
-def get_imd_movie_info(movie):
-    movie_title = movie.a.text
-    movie_year = movie.text.split()[-1].replace('(', '').replace(')', '')
-    movie_url = 'http://www.imdb.com' + movie.a['href']
-    movie_position = movie.text.split()[0].replace('.', '')
-    return movie_title, movie_year, movie_url, movie_position
+def get_movie_table_rows(movie):
+    title = movie.a.text
+    year = movie.text.split()[-1].replace('(', '').replace(')', '')
+    movie_url = urljoin(url, movie.a['href'])
+    position = movie.text.split()[0].replace('.', '')
+    return title, movie_url, year, position
 
 
-def writing_data(movie_title, movie_url, movie_year, image_url, movie_position, rating):
-    my_dict = {'Title': movie_title, 'Film url': movie_url, 'Year': movie_year, 'Image url': image_url,
-               'Position': movie_position, 'Rating': rating}
-
-    json_object = json.dumps(my_dict, indent=4) # encoding problems
-
-    # example
-    #string = json.loads(b'"\\u041a\\u0435\\u0439\\u0442\\u043b\\u0438\\u043d\\u043f\\u0440\\u043e"'.decode('utf8'))
-    #print(string)
-
-    with open("movies.json", "w") as outfile:
-        outfile.write(json_object)
+def extract_movie_props(row):
+    title, movie_url, year, image_url, position,  rating = parse_movie_row(row)
+    return {'Title': title, 'Film url': movie_url, 'Year': year, 'Image url': image_url,
+            'Position': position, 'Rating': rating}
 
 
-def imd_movie_picker():
-    rows = get_movie_soup()
-    for i in rows:
-        movie, image_url, rating = get_imd_movies(i)
-        movie_title, movie_year, movie_url, movie_position = get_imd_movie_info(movie)
-        if movie_year > '2000':
-            writing_data(movie_title, movie_url, movie_year, image_url, movie_position, rating)
+def main():
+    movies_data = list()
+
+    with open("movies.json", "w", encoding='utf8') as f:
+        rows = get_movie_soup()
+        for row in rows:
+            movie_props = extract_movie_props(row)
+            if int(movie_props['Year']) > 2000:
+                movies_data.append(movie_props)
+        json.dump(movies_data, f, indent=2)
 
 
 if __name__ == '__main__':
-    imd_movie_picker()
+    main()
