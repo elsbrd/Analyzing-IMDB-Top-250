a2ce8cdf9d94c4d35613c7c29c99daf1394d55a8
elsbrd <56909624+elsbrd@users.noreply.github.com>
2022-08-18 19:38:24 +0300

    Update movie_info_scaper

--- movie_info_scaper.py
+++ movie_info_scaper.py
@@ -1,65 +1,60 @@
-import requests
 import json
 import asyncio
+import aiohttp
+import time
 from bs4 import BeautifulSoup
 
-
-def get_data_from_file():
-    movie_url = list()
-    with open('movies.json') as f:
-        data = json.load(f)
-        for row in data:
-            movie_url.append(row['movie_url'])
-    return movie_url
+start_time = time.time()
 
 
-async def create_schedule():
-    for i in get_data_from_file():
-        await extract_movie_props(i)
+async def get_data_from_file():
+    movie_url = set()
+    async with aiohttp.ClientSession() as session:
+        with open('movies.json') as f:
+            data = json.load(f)
+            for url in data:
+                movie_url.add(asyncio.ensure_future(get_movie_section_tag(session, url['movie_url'])))
+            get_section_tags = await asyncio.gather(*movie_url)
+            for section_tags in get_section_tags:
+                print(parse_data(section_tags))
 
 
-def get_movie_tags(row):
-    response = requests.get(row)
-    soup = BeautifulSoup(response.text, 'html.parser')
-    movie_tags = soup.find('section', attrs={
-        'class': 'ipc-page-section ipc-page-section--baseAlt ipc-page-section--tp-none ipc-page-section--bp-xs '
-                 'sc-2a827f80-1 gvCXlM'})
-    return movie_tags
+async def get_movie_section_tag(session, url):
+    async with session.get(url) as response:
+        soup = BeautifulSoup(await response.text(), 'html.parser')
+        movie_tags = soup.find('section', attrs={
+            'class': 'ipc-page-section--baseAlt'})
+        return movie_tags
 
 
-def parse_data(row):
-    movie_tags = get_movie_tags(row)
-    title = movie_tags.find('div', class_='sc-80d4314-1 fbQftq').h1.text
-    year = movie_tags.find('div', class_='sc-80d4314-1 fbQftq').a.text
-    hours = list(map(lambda x: x.text, movie_tags.find_all('li', class_="ipc-inline-list__item")))[5]
-    rating = movie_tags.find('div', class_='sc-db8c1937-1 kVSEMR').span.text
-    popularity = movie_tags.find('div', class_="sc-edc76a2-1 gopMqI").text
-    info_film_type = list(
-        map(lambda x: x.span.text, movie_tags.find_all('a', class_='sc-16ede01-3 bYNgQ ipc-chip ipc-chip--on-baseAlt')))
-    summary = movie_tags.find('span', class_='sc-16ede01-0 fMPjMP').text
-    get_movie_info = movie_tags.find_all('ul',
-                                         class_='ipc-inline-list ipc-inline-list--show-dividers '
-                                                'ipc-inline-list--inline ipc-metadata-list-item__list-content baseAlt')
-    director = list(map(lambda x: x.a.text, get_movie_info[0]))
-    writers = list(map(lambda x: x.a.text, get_movie_info[1]))
-    stars = list(map(lambda x: x.a.text, get_movie_info[2]))
-    return title, year, hours, rating, popularity, info_film_type, summary, director, writers, stars
-
-
-async def extract_movie_props(row):
-    title, year, hours, rating, popularity, info_film_type, summary, director, writers, stars = parse_data(row)
+def parse_data(section_tags):
+    title = section_tags.h1.text
+    year = [year.span.text for year in section_tags][1]
+    hours = section_tags.find_all('li', class_="ipc-inline-list__item")[5].text
+    rating = section_tags.find('div', class_='sc-80d4314-3').span.text
+    popularity = section_tags.find('div', class_="sc-edc76a2-1 gopMqI").text
+    genres = [genre.span.text for genre in
+              section_tags.find_all('a', class_='sc-16ede01-3 bYNgQ ipc-chip ipc-chip--on-baseAlt')]
+    summary = section_tags.find('span', class_='gXUyNh').text
+    get_movie_info = section_tags.find_all('ul',
+                                           class_='ipc-inline-list ipc-inline-list--show-dividers '
+                                                  'ipc-inline-list--inline ipc-metadata-list-item__list-content baseAlt')
+    directors = [director.a.text for director in get_movie_info[0]]
+    writers = [writer.a.text for writer in get_movie_info[1]]
+    stars = [star.a.text for star in get_movie_info[2]]
     return {title: {
         'year': year,
         'hours': hours,
         'rating': rating,
         'popularity': popularity,
-        'info_film_type': info_film_type,
+        'genres': genres,
         'summary': summary,
-        'director': director,
+        'director': directors,
         'writers': writers,
         'stars': stars
     }}
 
 
 if __name__ == '__main__':
-    asyncio.run(create_schedule())
+    asyncio.run(get_data_from_file())
+    print("--- %s seconds ---" % (time.time() - start_time))
